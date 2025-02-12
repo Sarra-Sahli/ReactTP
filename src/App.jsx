7233d0b49112f6742e5c2a27dfb143db03698d3c
Sarra-Sahli <sarah1sahlii1@gmail.com>
2025-02-12 18:02:55 +0100

    atelier Event

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import Form from './components/Form'; // Importez Form depuis le bon chemin
 import Counter  from './components/counter';
 import ComFct from "./components/ComFct";
 import Event from "./components/Event"
+import Events from './components/Events';
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-    <Event />
+    <Events />
+
     </>
   );
 }
--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,15 +1,26 @@
 import { Card, Button } from "react-bootstrap";
 
-function Event({ name, description, img, price, nbTickets, nbParticipants, like }) {
+function Event({ name, description, img, price, nbTickets, nbParticipants, buy, like, toggleLike }) {
+  const placesRestantes = nbTickets - nbParticipants;
+  const isSoldOut = placesRestantes === 0;
+
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={img} />
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={`${img}`} />
       <Card.Body>
-        <Card.Title>name :{name}</Card.Title>
-        <Card.Text>description: {description}</Card.Text>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>{description}</Card.Text>
         <Card.Text>Prix : {price}€</Card.Text>
-        <Card.Text>Places disponibles : {nbTickets - nbParticipants}</Card.Text>
-        <Button variant={like ? "danger" : "success"}>
+        <Card.Text>
+          Places disponibles :{" "}
+          {isSoldOut ? <span style={{ color: "red" }}>Sold Out</span> : placesRestantes}
+        </Card.Text>
+
+        <Button variant="primary" onClick={buy} disabled={isSoldOut}>
+          {isSoldOut ? "Sold Out" : "Book an event"}
+        </Button>{" "}
+
+        <Button variant={like ? "danger" : "success"} onClick={toggleLike}>
           {like ? "Dislike" : "Like"}
         </Button>
       </Card.Body>
